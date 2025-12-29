5ee5a97e4ccc80e9ccea305d58d0250fb7891692
ArthurCoget <arthur.coget01@gmail.com>
2025-12-30 00:14:42 +0100

    Home page cleaned up

--- src/app/data/home.data.ts
+++ src/app/data/home.data.ts
@@ -56,3 +56,25 @@ export const CAROUSEL_IMAGES: ICarouselImage[] = [
   { id: 4, img: '/placeholder/4-placeholder.webp', title: 'City Loft', date: '11/2021' },
   { id: 5, img: '/placeholder/5-placeholder.webp', title: 'Garden Office', date: '05/2022' },
 ];
+
+export const HOME_CONTENT = {
+  topContainerTextBlue:
+    'Groen bouwen of traditioneel of een combinatie van beiden? Energiezuinige woning of appartement kopen? Milieubewust renoveren of inrichten?',
+  topContainerText:
+    'Duurzame aannemer Thorbouw deelt met véél plezier zijn eindeloze passie voor vol te houden woonvormen oftewel ‘volhoudbaar wonen’. Wij bouwen, renoveren en ontwikkelen voor een vaste prijs en volgens uw budget uw zorgeloze thuis en een duurzame toekomst in Vlaams-Brabant. ',
+  middleTitle: 'Van meubel tot verbouwingen met totaalinrichting.',
+  middleText:
+    'Hebt u interesse? Neem zeker een kijkje door onze vorige projecten voor wat meer informatie.',
+  bottomTitle: 'Wist u dat?',
+  bottomTextIntro:
+    'De Zuid-Afrikaanse taal zit boordevol warme termen, zoals volhoudbaar wat letterlijk duurzaam betekent. Met een volhoudbare aanpak respecteren ze oorsprong, houden ze vol in hetnu en zorgen ze dat het houdbaar blijft voor wie na hen komt.',
+  bottomTextMiddle:
+    'Het verschil tussen duurzaam en volhoudbaar? Duurzaam is niet noodzakelijk ecologisch. Denk bijvoorbeeld aan tuinmeubilair uit gerecycleerde pvc in je tuin. Na jaren buiten in weer en wind zit uw tuin vol microplastic. Thorbouw licht alle voor- en nadelen toe van de verschillende materialen, zodat je als klant een voor jezelf verantwoorde keuze kan maken. Wie kiest voor een volhoudbare aanpak, maakt de juiste keuze voor zichzelf, de volgende generaties en onze planeet. Deze wijze spreuk van de Canadese Haida indianen vat het perfect samen: „We erven de wereld niet van onze ouders, maar lenen de wereld van onze kinderen.”',
+  bottomTextEnd:
+    'Als aannemersbedrijf in Leuven zetten wij 100% in op toekomstgericht wonen. We adviseren haalbare ingrepen, betaalbare energie-oplossingen en zowel traditionele als natuurlijke bouwmaterialen. Ethiek en integriteit houden wij hoog in het vaandel, onze klanten correct informeren maakt deel uit van onze werkwijze.',
+  bottomTextConclusionBeforeLink:
+    'Algemeen aannemer en binnenhuisarchitect Jan Vansteenwegen begeleidt u, samen met de ',
+  bottomTextConclusionAfterLink:
+    ', naar een huis, appartement, kangoeroewoning of nieuwbouwproject met respect voor onze planeet en met veel liefde voor de volgende generatie Leuvenaars. Wij bieden u het volledige totaalpakket: woningbouw, verbouwing, inrichting, meubilair en tuin. Volledig op uw maat en met oog voor het milieu.',
+  bottomTextConlcusionLink: 'North South Architecten',
+};
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero-component/hero-component';
 import { PillarsComponent } from '../../components/pillars-component/pillars-component';
 import { ImageCarouselComponent } from '../../components/image-carousel-component/image-carousel-component';
-import { HOME_HERO_CONTENT, HOME_PILLARS, CAROUSEL_IMAGES } from '../../data/home.data';
+import {
+  HOME_HERO_CONTENT,
+  HOME_PILLARS,
+  CAROUSEL_IMAGES,
+  HOME_CONTENT,
+} from '../../data/home.data';
 import { NgOptimizedImage } from '@angular/common';
 
 @Component({
@@ -15,4 +20,5 @@ export class Home {
   readonly heroContent = HOME_HERO_CONTENT;
   readonly pillars = HOME_PILLARS;
   readonly carouselImages = CAROUSEL_IMAGES;
+  readonly homeContent = HOME_CONTENT;
 }
