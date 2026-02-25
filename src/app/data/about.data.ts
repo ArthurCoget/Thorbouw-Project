57212635c1cf28659db3a91c8659e8f0ee0d3324
ArthurCoget <arthur.coget01@gmail.com>
2026-02-25 12:59:07 +0100

    about page in progress

---
+++ src/app/data/about.data.ts
@@ -0,0 +1,7 @@
+export const ABOUT_CONTENT = {
+  aboutTopTitle: 'Wie Zijn We?',
+  aboutTopSubText:
+    'Welkom bij Thorbouw, uw betrouwbare bouwpartner en algemeen aannemersbedrijf met een visie op de toekomst en een groot hart voor de volgende generaties. Persoonlijk en milieubewust aankopen, bouwen, renoveren, inrichten of ontwikkelen in Leuven en ruime omgeving. Ruime ervaring in zowel traditionele als bio-ecologische bouwtechnieken.',
+  numberOfProjectsLabel: 'Afgeleverde Projecten',
+  numberOfProjects: '26',
+};
--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero-component/hero-component';
 import { IHeroContent } from '../../interfaces/hero-content';
+import { ABOUT_CONTENT } from '../../data/about.data';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-about',
-  imports: [HeroComponent],
+  imports: [HeroComponent, NgOptimizedImage],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
@@ -15,4 +17,6 @@ export class About {
     altText: 'Thorbouw team working on sustainable construction project',
     title: 'Over Thorbouw',
   };
+
+  readonly aboutContent = ABOUT_CONTENT;
 }
