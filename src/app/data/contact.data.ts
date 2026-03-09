77daa814d718737c39b846b95db5d0246abf3603
ArthurCoget <arthur.coget01@gmail.com>
2026-03-09 18:15:09 +0100

    Setup for cards ready

---
+++ src/app/data/contact.data.ts
@@ -0,0 +1,13 @@
+import { IHeroContent } from '../interfaces/hero-content';
+
+export const HERO_CONTACT_CONTENT: IHeroContent = {
+  variant: 'simple',
+  image: 'ContactHero.webp',
+  altText: 'Thorbouw',
+  title: 'Contact',
+};
+export const CONTACT_CONTENT = {
+  contactTitle: 'Neem zeker contact op, indien u vragen heeft zijn wij er om u te helpen.',
+  contactSubTitle:
+    'We zijn alle dagen van de week open en in het weekend zijn we in de voormiddag berijkbaar.',
+};
--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,18 +1,17 @@
-import { Component, SimpleChange } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero-component/hero-component';
-import { IHeroContent } from '../../interfaces/hero-content';
+import { CONTACT_CONTENT, HERO_CONTACT_CONTENT } from '../../data/contact.data';
+import { ContactCard } from '../../components/contact-card/contact-card';
 
 @Component({
   selector: 'app-contact',
-  imports: [HeroComponent],
+  imports: [HeroComponent, ContactCard],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
-  readonly heroContent: IHeroContent = {
-    variant: 'simple',
-    image: 'ContactHero.webp',
-    altText: 'Thorbouw',
-    title: 'Contact',
-  };
+  readonly heroContent = HERO_CONTACT_CONTENT;
+  readonly contactContent = CONTACT_CONTENT;
+
+  readonly list = [1, 2, 3, 4, 5];
 }
