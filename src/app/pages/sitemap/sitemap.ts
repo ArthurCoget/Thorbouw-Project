54e66b9fca8e1a0f914ad09543e21245b53934dd
ArthurCoget <arthur.coget01@gmail.com>
2026-03-13 16:53:28 +0100

    Out of scope added sitemap page, could generate xml + robots.txt for google crawlers

--- src/app/pages/sitemap/sitemap.ts
+++ src/app/pages/sitemap/sitemap.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IHeroContent } from '../../interfaces/hero-content';
-import { HeroComponent } from '../../components/hero-component/hero-component';
 
 @Component({
   selector: 'app-sitemap',
-  imports: [RouterLink, HeroComponent],
+  imports: [RouterLink],
   templateUrl: './sitemap.html',
   styleUrl: './sitemap.css',
 })
-export class Sitemap {
-  readonly heroContent: IHeroContent = {
-    variant: 'simple',
-    image: 'AboutHeroPage.webp',
-    altText: 'Thorbouw team working on sustainable construction project',
-    title: 'Sitemap',
-  };
-}
+export class Sitemap {}
