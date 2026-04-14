dad9071096d8fe391633da6d99cf08d732eff02a
ArthurCoget <arthur.coget01@gmail.com>
2026-04-14 16:17:04 +0200

    Added content for Justisius Lupsius + refactor for images in IProjectContent

--- src/app/data/projects.data.ts
+++ src/app/data/projects.data.ts
@@ -1,4 +1,95 @@
-import { IProjectContent } from '../interfaces/iproject-content';
+import { IProjectContent, createImages } from '../interfaces/iproject-content';
+
+const imagesJusiusLipsius = [
+  // 1 - 20 (Exterieur)
+  ...createImages(
+    1,
+    20,
+    '/fotos&realisaties/JustiusLipsius',
+    'Foto van renovatie in Leuven exterieur',
+    {
+      category: 'Exterieur',
+      prefix: 'Justius',
+    },
+  ),
+
+  // 1 - 8 (Hal)
+  ...createImages(
+    1,
+    8,
+    '/fotos&realisaties/JustiusLipsius',
+    'Foto van renovatie in Leuven interieur hal',
+    {
+      category: 'Interieur',
+      title: 'Hal',
+      prefix: 'Lipsius',
+    },
+  ),
+
+  // 9 - 14 (Eettafel)
+  ...createImages(
+    9,
+    14,
+    '/fotos&realisaties/JustiusLipsius',
+    'Foto van renovatie in Leuven interieur eettafel',
+    {
+      category: 'Interieur',
+      title: 'Eettafel',
+      prefix: 'Lipsius',
+    },
+  ),
+  // 15 - 19 (Keuken)
+  ...createImages(
+    15,
+    19,
+    '/fotos&realisaties/JustiusLipsius',
+    'Foto van renovatie in Leuven interieur overzicht living',
+    {
+      category: 'Interieur',
+      title: 'Overzicht Living',
+      prefix: 'Lipsius',
+    },
+  ),
+
+  // 20 - 21 (Overzicht Living)
+  ...createImages(
+    20,
+    21,
+    '/fotos&realisaties/JustiusLipsius',
+    'Foto van renovatie in Leuven interieur overzicht living',
+    {
+      category: 'Interieur',
+      title: 'Overzicht Living',
+      prefix: 'Lipsius',
+    },
+  ),
+
+  // 22 - 24 (Salon)
+  ...createImages(
+    22,
+    24,
+    '/fotos&realisaties/JustiusLipsius',
+    'Foto van renovatie in Leuven interieur salon',
+    {
+      category: 'Interieur',
+      title: 'Salon',
+      prefix: 'Lipsius',
+    },
+  ),
+
+  // 20 (Slaapkamer)
+  ...createImages(
+    25,
+    25,
+    '/fotos&realisaties/JustiusLipsius',
+    'Foto van renovatie in Leuven interieur slaapkamer',
+    {
+      category: 'Interieur',
+      title: 'Slaapkamer',
+      prefix: 'Lipsius',
+    },
+  ),
+];
 
 export const PROJECTS: IProjectContent[] = [
   {
@@ -128,5 +219,29 @@ export const PROJECTS: IProjectContent[] = [
       src: `/fotos&realisaties/Herent/Herent_${i + 1}.webp`,
       alt: 'Foto van nieuwbouw in Herent',
     })),
+  },
+  {
+    slug: 'jusiuslipsius',
+    title: 'Renovatie voor Jusius Lipsius',
+    subtitle: 'Historische renovatie met respect voor erfgoed en moderne woonbehoeften',
+    description:
+      'Voor de renovatie van deze historische woning in Leuven combineerden we vakmanschap met moderne technieken om een comfortabele leefruimte te creëren, terwijl we het karakter en de charme van het oorspronkelijke gebouw behouden.',
+    location: 'Leuven',
+    year: 2024,
+    cardImages: [
+      {
+        src: '/fotos&realisaties/JusiusLipsius/JusiusLipsius_1.webp',
+        alt: 'Foto van renovatie in Leuven',
+      },
+      {
+        src: '/fotos&realisaties/JusiusLipsius/Lipsius_1.webp',
+        alt: 'Foto van renovatie in Leuven',
+      },
+      {
+        src: '/fotos&realisaties/JusiusLipsius/Justius_15.webp',
+        alt: 'Foto van renovatie in Leuven',
+      },
+    ],
+    images: imagesJusiusLipsius,
   },
 ];
--- src/app/interfaces/iproject-content.ts
+++ src/app/interfaces/iproject-content.ts
@@ -16,3 +16,24 @@ export interface IProjectContent {
   cardImages: IProjectImage[];
   images: IProjectImage[];
 }
+
+export function createImages(
+  start: number,
+  end: number,
+  basePath: string,
+  alt: string,
+  options: {
+    category?: string;
+    title?: string;
+    description?: string;
+    prefix: string;
+  },
+): IProjectImage[] {
+  return Array.from({ length: end - start + 1 }, (_, i) => ({
+    src: `${basePath}/${options.prefix}_${start + i}.webp`,
+    alt: alt,
+    category: options.category,
+    ...(options.title && { title: options.title }),
+    ...(options.description && { description: options.description }),
+  }));
+}
