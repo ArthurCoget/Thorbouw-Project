67d5a447ea596c7d3b7b5b3df83b1a2c623b063d
ArthurCoget <arthur.coget01@gmail.com>
2026-04-18 14:32:44 +0200

    Removed placeholder + automatically update imagecarousel home with projects

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,13 +2,10 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero-component/hero-component';
 import { PillarsComponent } from '../../components/pillars-component/pillars-component';
 import { ImageCarouselComponent } from '../../components/image-carousel-component/image-carousel-component';
-import {
-  HOME_HERO_CONTENT,
-  HOME_PILLARS,
-  CAROUSEL_IMAGES,
-  HOME_CONTENT,
-} from '../../data/home.data';
+import { HOME_HERO_CONTENT, HOME_PILLARS, HOME_CONTENT } from '../../data/home.data';
 import { NgOptimizedImage } from '@angular/common';
+import { PROJECTS } from '../../data/projects.data';
+import { ICarouselImage } from '../../interfaces/carousel-image';
 
 @Component({
   selector: 'app-home',
@@ -19,6 +16,17 @@ import { NgOptimizedImage } from '@angular/common';
 export class Home {
   readonly heroContent = HOME_HERO_CONTENT;
   readonly pillars = HOME_PILLARS;
-  readonly carouselImages = CAROUSEL_IMAGES;
   readonly homeContent = HOME_CONTENT;
+  readonly projects = PROJECTS;
+  readonly carouselImages = this.setupCarouselImages();
+
+  private setupCarouselImages(): ICarouselImage[] {
+    return this.projects.map((project, index) => ({
+      id: index + 1,
+      img: project.images[0].src,
+      title: project.title,
+      date: project.year.toString(),
+      slug: project.slug,
+    }));
+  }
 }
