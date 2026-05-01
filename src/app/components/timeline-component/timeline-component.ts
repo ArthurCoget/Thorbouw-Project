17f341e829ce4fa93353351f45b6380603a53a3b
ArthurCoget <arthur.coget01@gmail.com>
2026-05-01 21:20:18 +0200

    time-line component finished

---
+++ src/app/components/timeline-component/timeline-component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-timeline-component',
+  imports: [RouterLink],
+  templateUrl: './timeline-component.html',
+  styleUrl: './timeline-component.css',
+})
+export class TimelineComponent {
+  @Input({ required: true }) titleAndDescriptionAndButton!: {
+    title: string;
+    description: string;
+    buttonText: string;
+    buttonLink: string;
+    buttonFragment: string;
+  };
+
+  @Input({ required: true }) items!: {
+    title: string;
+    description: string;
+  }[];
+}
---
+++ src/app/data/werkwijze.data.ts
@@ -0,0 +1,36 @@
+export const TIMELINE_TITLE_AND_DESCRIPTION_AND_BUTTON = {
+  title: 'Thorbouw werkwijze process',
+  description:
+    'We willen uw ervaringen in de renovatie van uw woning zo goed mogelijk maken. Daarom hebben we een duidelijk proces ontwikkeld dat u stap voor stap begeleidt. Van de eerste kennismaking tot de oplevering en nazorg, we staan altijd klaar om u te ondersteunen en te zorgen dat alles soepel verloopt.',
+  buttonText: 'Lees meer',
+  buttonLink: '/werkwijze',
+  buttonFragment: 'first-step',
+};
+
+export const TIMELINE_ITEMS = [
+  {
+    title: 'Eerste Kennismaking',
+    description:
+      'Laten we elkaar eens leren kennen. We bespreken uw visie en hoe we kunnen samenwerken. We geven u ook een eerste prijsindicatie, zodat we weten of we op één lijn zitten.',
+  },
+  {
+    title: 'Ontwerp en Offerte',
+    description:
+      'We ontwerpen samen uw renovatie en doen een offerte. U krijgt een duidelijk overzicht van de kosten en de uitvoering.',
+  },
+  {
+    title: 'Uitvoering',
+    description:
+      'We starten de renovatie en houden u op de hoogte van de voortgang. We zorgen ervoor dat alles volgens plan verloopt.',
+  },
+  {
+    title: 'Oplevering',
+    description:
+      'We leveren het project op en zorgen ervoor dat alles naar wens is. We staan klaar om eventuele vragen te beantwoorden en zorgen voor een soepele overgang.',
+  },
+  {
+    title: 'Nazorg',
+    description:
+      'We bieden nazorg en ondersteuning na de oplevering. We willen ervoor zorgen dat u tevreden bent met het resultaat en staan klaar om eventuele problemen op te lossen.',
+  },
+];
